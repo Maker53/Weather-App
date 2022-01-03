2e81577e66d01131e0c0e4d46a839310c2013163
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-03 20:01:47 +0300

    Add services (NetworkManager)

---
+++ Services/NetworkWeatherManager.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkWeatherManager.swift
+//  Weather App
+//
+//  Created by Станислав on 03.01.2022.
+//
+import UIKit
+
+struct NetworkWeatherManager {
+    func fetchCurrentWeather(forCity city: String) {
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(APIKey)"
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let dataString = String(data: data, encoding: .utf8)
+                print(dataString!)
+            }
+        }
+        task.resume()
+    }
+}
