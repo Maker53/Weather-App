92d08b3b8d7f664eaad9c4f4042c963d2ee5c92d
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-04 12:25:08 +0300

    Add model for parse JSON - CurrentWeatherData

---
+++ Weather App/Models/CurrentWeatherData.swift
@@ -0,0 +1,26 @@
+//
+//  CurrentWeatherData.swift
+//  Weather App
+//
+//  Created by Станислав on 04.01.2022.
+//
+
+struct CurrentWeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+}
